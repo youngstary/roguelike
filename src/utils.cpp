4e2d8180deee7825a295c83e1a27894208ce4cfd
sebastian.bartosik <sbartosik17@gmail.com>
2023-06-30 20:22:23 +0200

    Generating an item room and a shop. Not working yet.

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,21 +1,16 @@
-#include <stdio.h>
-#include <cstring>
-#include <cstdlib>
 #include "raylib.h"
+#include <cstdlib>
+#include <cstring>
+#include <stdio.h>
 
 #include "utils.h"
 
-Map::Map()
-{
-}
+Map::Map() {}
 
-void Map::Print()
-{
+void Map::Print() {
     printf("---ARRAY---\n");
-    for (int i = 0; i < 10; i++)
-    {
-        for (int j = 0; j < 10; j++)
-        {
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
             printf("%d", map[i][j]);
         }
         printf("\n");
@@ -23,8 +18,7 @@ void Map::Print()
     printf("-----------\n");
 }
 
-void Map::Generate()
-{
+void Map::Generate() {
     std::memset(map, 0, (10 * 10) * (sizeof *map));
     const int level = 1;
 
@@ -33,8 +27,7 @@ void Map::Generate()
 
     int bossX, bossY, dx, dy;
 
-    do
-    {
+    do {
         bossX = GetRandomValue(0, 9);
         bossY = GetRandomValue(0, 9);
         dx = abs(bossX - x);
@@ -46,11 +39,10 @@ void Map::Generate()
 
     int horizontal = dx;
     int vertical = dy - 1; // One vertical move counts also as horizontal move
-    const int moves = horizontal + vertical;
+    int moves = horizontal + vertical;
     int sequence[moves];
 
-    for (int i = 0; i < moves; i++)
-    {
+    for (int i = 0; i < moves; i++) {
         int choice;
 
         if (horizontal == 0)
@@ -60,16 +52,13 @@ void Map::Generate()
         else
             choice = GetRandomValue(HORIZONTAL, VERTICAL);
 
-        if (choice == HORIZONTAL)
-        {
+        if (choice == HORIZONTAL) {
             if (x < bossX)
                 sequence[i] = RIGHT;
             else
                 sequence[i] = LEFT;
             horizontal--;
-        }
-        else if (choice == VERTICAL)
-        {
+        } else if (choice == VERTICAL) {
             if (y < bossY)
                 sequence[i] = DOWN;
             else
@@ -78,8 +67,12 @@ void Map::Generate()
         }
     }
 
-    for (int move : sequence)
-    {
+    bool canCreateItemRoom = true;
+    bool canCreateShop = true;
+    int createItemRoom;
+    int createShop;
+
+    for (int move : sequence) {
         if (move == UP)
             y--;
         else if (move == RIGHT)
@@ -88,6 +81,57 @@ void Map::Generate()
             y++;
         else if (move == LEFT)
             x--;
+
         map[y][x] = NORMAL;
+
+        if (canCreateItemRoom)
+            createItemRoom = GetRandomValue(1, moves);
+
+        if (canCreateShop)
+            createShop = GetRandomValue(1, moves);
+
+        if (createItemRoom == 1) {
+            MoveRandomDirection(x, y, ITEM);
+            canCreateItemRoom = false;
+            createItemRoom = -1;
+        }
+
+        if (createShop == 1) {
+            MoveRandomDirection(x, y, SHOP);
+            canCreateShop = false;
+            createShop = -1;
+        }
+
+        moves--;
     }
+
+    Map::Print();
+}
+
+int Map::GetRandomDirection(int x, int y) {
+    while (true) {
+        int dir = GetRandomValue(UP, LEFT);
+
+        if (dir == UP && y != 0 && map[y - 1][x] == 0)
+            return UP;
+        else if (dir == RIGHT && x != 10 && map[y][x + 1] == 0)
+            return RIGHT;
+        else if (dir == DOWN && y != 10 && map[y + 1][x] == 0)
+            return DOWN;
+        else if (dir == LEFT && x != 0 && map[y][x - 1] == 0)
+            return LEFT;
+    }
+}
+
+void Map::MoveRandomDirection(int x, int y, int room) {
+    int dir = Map::GetRandomDirection(x, y);
+
+    if (dir == UP)
+        map[y - 1][x] = room;
+    else if (dir == RIGHT)
+        map[y][x + 1] = room;
+    else if (dir == DOWN)
+        map[y + 1][x] = room;
+    else if (dir == LEFT)
+        map[y][x - 1] = room;
 }
--- src/utils.h
+++ src/utils.h
@@ -1,31 +1,16 @@
-#include <stdio.h>
 #include "raylib.h"
+#include <stdio.h>
 
-class Map
-{
-public:
+class Map {
+  public:
     Map();
     void Print();
     void Generate();
-    
+    int GetRandomDirection(int x, int y);
+    void MoveRandomDirection(int x, int y, int room);
     int map[10][10] = {0};
 
-    enum Dir
-    {
-        UP,
-        RIGHT,
-        DOWN,
-        LEFT,
-        HORIZONTAL,
-        VERTICAL
-    };
+    enum Dir { UP, RIGHT, DOWN, LEFT, HORIZONTAL, VERTICAL };
 
-    enum Room
-    {
-        NONE,
-        NORMAL,
-        ITEM,
-        SHOP,
-        BOSS
-    };
+    enum Room { NONE, NORMAL, ITEM, SHOP, BOSS };
 };
