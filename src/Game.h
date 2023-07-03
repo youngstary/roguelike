9ddaf1a27c332c65969332003008ab9c49d9a0f8
antek-bizon <antek.bizon@gmail.com>
2023-07-03 10:45:37 +0200

    Gui bug

--- src/Game.h
+++ src/Game.h
@@ -4,13 +4,14 @@
 #include "raylib.h"
 
 #include "Components.h"
+#include "Gui.h"
 #include "Player.h"
 #include "StaticObjects.h"
 
-class Game {
-  public:
+struct Game {
     Game(int screenWidth, int screenHeight);
     ~Game();
+
     int MainLoop();
     void Update();
     void UpdateCamera2D(Vector2 targetPos);
@@ -19,8 +20,14 @@ class Game {
     void DestroyLevel();
     void CheckCollisions();
     void PlayerCollisions();
+    void ChangeState(State nextState);
+    void TooglePause(State nextState);
 
     Player player;
     std::vector<Wall> walls;
     Camera2D camera = {0};
+    State currentState;
+    State nextState = currentState;
+
+    Gui gui;
 };
